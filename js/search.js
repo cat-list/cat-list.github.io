1ac07e6850d842453380ba30155f51565b7a541c
Thomas Chaplin <tomrchaplin@gmail.com>
2023-03-16 12:57:26 +0000

    Improve search functionality

--- js/search.js
+++ js/search.js
@@ -8,51 +8,78 @@ var STATE = {};
 
 const UPDATE = (function () {
   function updateSearchBox() {
-    document.getElementById('search-box').value = STATE.search_term;
+    document.getElementById('search-box').value = STATE.query;
   }
 
   function updateURLBar() {
-    const query_uri = encodeURIComponent(STATE.search_term);
-    const query_string = query_uri.length == 0 ? '/' : '/?query=' + query_uri;
-    window.history.replaceState({ query: query_uri }, '', query_string);
+    const history_state = {
+      query: STATE.query,
+      filter_tags: STATE.filter_tags,
+    };
+    const usp = new URLSearchParams(history_state);
+    const query_string = '/?' + usp.toString();
+    window.history.replaceState(history_state, '', query_string);
   }
 
-  function runSearch() {
-    function buildQuery(search_term) {
-      query = [];
-      terms = search_term.split(' ');
-      terms.forEach((term) => {
-        var term = term.trim();
-        if (term.includes(':')) {
-          query.push(term);
-        } else {
-          query.push(search_term);
-          query.push('*' + search_term);
-          query.push(search_term + '*');
-          query.push('*' + search_term + '*');
-        }
-      });
-      return query.join(' ');
-    }
-    var refs;
-    if (STATE.search_term == '') {
-      // Use default order on empty search
-      refs = STATE.shadow.map((node) => node.id);
+  function updateFilterBar() {
+    const search_hint = document.getElementById('search-hint');
+    const filter_tags = document.getElementById('filter-tags');
+    if (STATE.filter_tags.length == 0) {
+      filter_tags.classList.add('hidden');
+      search_hint.classList.remove('hidden');
+      filter_tags.innerHTML = '';
     } else {
-      const query = buildQuery(STATE.search_term);
-      refs = STATE['idx'].search(query).map((res) => res.ref);
+      search_hint.classList.add('hidden');
+      filter_tags.innerHTML = '';
+      STATE.filter_tags.forEach((f_tag) => {
+        let elem = document.createElement('span');
+        let main_tag = f_tag.split('/')[0];
+        elem.classList.add('TAG_' + main_tag);
+        elem.classList.add('pill');
+        let delete_elem = document.createElement('span');
+        delete_elem.innerHTML = '🗑️';
+        delete_elem.classList.remove('delete_button');
+        delete_elem.onclick = (e) => {
+          console.log(STATE.filter_tags);
+          const index = STATE.filter_tags.indexOf(f_tag);
+          console.log(index);
+          if (index > -1) {
+            STATE.filter_tags.splice(f_tag, 1);
+            UPDATE();
+          }
+        };
+        elem.innerHTML = f_tag + ' ';
+        elem.appendChild(delete_elem);
+        filter_tags.appendChild(elem);
+      });
+      filter_tags.classList.remove('hidden');
+    }
+  }
+
+  // TODO: Make use of res.refIndex from fuse search
+  function runSearch() {
+    function idxContainsAllTags(idx) {
+      return STATE.filter_tags.every((f_tag) => {
+        return STATE.data[idx].tags.includes(f_tag);
+      });
+    }
+
+    var idxs;
+    if (STATE.query == '') {
+      // Use default order on empty search
+      idxs = STATE.data.map((elem, idx) => idx);
+    } else {
+      idxs = STATE.fuse.search(STATE.query).map((res) => res.refIndex);
     }
     const table = document.getElementById('table_body');
     table.innerHTML = '';
-    refs.forEach((ref) => {
-      var new_node = STATE.shadow
-        .find((node) => node.id == ref)
-        .cloneNode(true);
+    idxs.filter(idxContainsAllTags).forEach((idx) => {
+      var new_node = STATE.shadow[idx].cloneNode(true);
       table.appendChild(new_node);
     });
   }
 
-  const callbacks = [updateSearchBox, updateURLBar, runSearch];
+  const callbacks = [updateSearchBox, updateURLBar, updateFilterBar, runSearch];
 
   return function () {
     callbacks.forEach((f) => {
@@ -61,30 +88,32 @@ const UPDATE = (function () {
   };
 })();
 
+function addTag(tag_name) {
+  STATE.filter_tags.push(tag_name);
+  UPDATE();
+}
+
 function SETUP() {
-  function getIndex() {
+  function getDataAndFuse() {
     return fetch(METADATA.urls.index)
       .then((res) => res.json())
       .then((data) => {
-        return lunr(function () {
-          this.ref('id');
-          this.field('name');
-          this.field('content');
-          this.field('tags');
-          data.forEach(function (doc) {
-            this.add(doc);
-          }, this);
-        });
-      })
-      .then((idx) => {
-        STATE.idx = idx;
+        const options = {
+          useExtendedSearch: true,
+          keys: ['name', 'title', 'content', 'tags'],
+        };
+
+        STATE.data = data;
+        STATE.fuse = new Fuse(data, options);
       });
   }
 
   function getTags() {
     return fetch(METADATA.urls.tags)
       .then((res) => res.json())
-      .then((tags) => (STATE.tags = tags));
+      .then((tags) => {
+        STATE.tags = tags;
+      });
   }
 
   function buildShadow() {
@@ -100,16 +129,41 @@ function SETUP() {
     const usp = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(usp.entries());
     if (params.query) {
-      STATE.search_term = decodeURIComponent(params.query);
+      STATE.query = decodeURIComponent(params.query);
     } else {
-      STATE.search_term = '';
+      STATE.query = '';
+    }
+    if (params.filter_tags) {
+      STATE.filter_tags = decodeURIComponent(params.filter_tags).split(',');
+    } else {
+      STATE.filter_tags = [];
     }
   }
 
   function attachSearchCallback() {
+    function addTag(tag_name) {
+      if (!STATE.tags.includes(tag_name)) {
+        return false;
+      }
+      if (!STATE.filter_tags.includes(tag_name)) {
+        STATE.filter_tags.push(tag_name);
+        return true;
+      } else {
+        return false;
+      }
+    }
+
     document.getElementById('search-box').oninput = (e) => {
-      STATE.search_term = e.target.value;
+      STATE.query = e.target.value;
       UPDATE();
+    };
+    document.getElementById('search-form').onsubmit = (e) => {
+      e.preventDefault();
+      if (addTag(STATE.query)) {
+        document.getElementById('search-box').value = '';
+        STATE.query = '';
+        UPDATE();
+      }
     };
   }
 
@@ -120,7 +174,7 @@ function SETUP() {
   return Promise.all([
     setInitialQuery(),
     buildShadow(),
-    getIndex(),
+    getDataAndFuse(),
     getTags(),
     focusSearchBox(),
     attachSearchCallback(),
